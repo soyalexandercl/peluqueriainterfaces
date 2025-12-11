fb93fb6c278258edcded11de7f80d0efe6f016e2
Jhon Alexander <jcl0040@alu.medac.es>
2025-12-11 01:04:18 +0100

    Nuevo codigo

--- src/main/java/modelo/Conexion.java
+++ src/main/java/modelo/Conexion.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 // Clase para gestionar la conexión a la base de datos PostgreSQL
 public class Conexion {
     // Ajusta estos datos a tu configuración local de PostgreSQL
-    private static final String URL = "jdbc:postgresql://localhost:5432/peluqueria_db";
+    private static final String URL = "jdbc:postgresql://localhost:5432/peluqueria";
     private static final String USUARIO = "postgres";
-    private static final String PASSWORD = "admin"; // Tu contraseña aquí
+    private static final String PASSWORD = "123"; // Tu contraseña aquí
 
     // Método estático para obtener la conexión
     public static Connection getConexion() {
